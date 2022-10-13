1cb3a27a6ef6e27dcffbe4e1199123708b836415
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-10-13 16:36:22 +0300

    initialize project, and installes MUI and Firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAy4dTQPEuxVpNvJDyE_WQ7OV4e9zQvisY",
+  authDomain: "quiz-game-6e76d.firebaseapp.com",
+  projectId: "quiz-game-6e76d",
+  storageBucket: "quiz-game-6e76d.appspot.com",
+  messagingSenderId: "658511477887",
+  appId: "1:658511477887:web:18b7b8c42c9df60c7f134a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
