280e829114ae5628da134d9fece297e72c1624b8
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-10-13 19:42:27 +0300

    added auth and add user info to firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import './App.css';
+import "./App.css";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+import { useEffect } from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -12,18 +15,55 @@ const firebaseConfig = {
   projectId: "quiz-game-6e76d",
   storageBucket: "quiz-game-6e76d.appspot.com",
   messagingSenderId: "658511477887",
-  appId: "1:658511477887:web:18b7b8c42c9df60c7f134a"
+  appId: "1:658511477887:web:18b7b8c42c9df60c7f134a",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const db = getDatabase();
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  useEffect(() => {
+    const signInAnonymouslyFirebase = () => {
+      onAuthStateChanged(auth, (user) => {
+        console.log(user);
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          const uid = user.uid;
+          // ...
+          writeUserData(uid, "nickname");
+        } else {
+          // User is signed out
+          // ...
+        }
+      });
+
+      signInAnonymously(auth)
+        .then(() => {
+          // Signed in..
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ...
+          console.log(errorCode, errorMessage);
+        });
+    };
+
+    const writeUserData = (userId, nickname) => {
+      set(ref(db, "players/" + userId), {
+        id: userId,
+        nickname,
+      });
+    };
+
+    signInAnonymouslyFirebase();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
