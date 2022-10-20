ebcdd54e624a7988057975d8744290357e6658b3
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-10-20 17:24:38 +0300

    write data in firebase when opening new room

--- src/App.js
+++ src/App.js
@@ -1,71 +1,15 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Homepage from "./components/Welcome/Homepage";
 import Main from "./components/Main/Main";
 import "./App.css";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
-import { getDatabase, ref, set } from "firebase/database";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAy4dTQPEuxVpNvJDyE_WQ7OV4e9zQvisY",
-  authDomain: "quiz-game-6e76d.firebaseapp.com",
-  projectId: "quiz-game-6e76d",
-  storageBucket: "quiz-game-6e76d.appspot.com",
-  messagingSenderId: "658511477887",
-  appId: "1:658511477887:web:18b7b8c42c9df60c7f134a",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-const db = getDatabase();
 
 function App() {
-  useEffect(() => {
-    const signInAnonymouslyFirebase = () => {
-      onAuthStateChanged(auth, (user) => {
-        console.log(user);
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          const uid = user.uid;
-          // ...
-          writeUserData(uid, "nickname");
-        } else {
-          // User is signed out
-          // ...
-        }
-      });
-
-      signInAnonymously(auth)
-        .then(() => {
-          // Signed in..
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // ...
-          console.log(errorCode, errorMessage);
-        });
-    };
-
-    const writeUserData = (userId, nickname) => {
-      set(ref(db, "players/" + userId), {
-        id: userId,
-        nickname,
-      });
-    };
-
-    signInAnonymouslyFirebase();
-  }, []);
+  // useEffect(() => {
+    
+  // }, []);
 
   return (
     <div className="App">
--- src/components/Welcome/CreateRoom.js
+++ src/components/Welcome/CreateRoom.js
@@ -10,6 +10,29 @@ import { randomNumber } from "../../util/index";
 import Superheroes from "superheroes";
 // import classes from "./Homepage.module.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import { getDatabase, ref, set } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAy4dTQPEuxVpNvJDyE_WQ7OV4e9zQvisY",
+  authDomain: "quiz-game-6e76d.firebaseapp.com",
+  projectId: "quiz-game-6e76d",
+  storageBucket: "quiz-game-6e76d.appspot.com",
+  messagingSenderId: "658511477887",
+  appId: "1:658511477887:web:18b7b8c42c9df60c7f134a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const db = getDatabase();
+
 const CreateRoom = () => {
   const [roomKey, setRoomKey] = useState("");
   const [generatedNickname, setGeneratedNickname] = useState("");
@@ -28,7 +51,7 @@ const CreateRoom = () => {
 
   const createRoomSubmitHandler = (event) => {
     event.preventDefault();
-    console.log(generatedNickname);
+
     let data;
     if (nickname === "" || nickname === null) {
       data = { roomKey, nickname: generatedNickname };
@@ -36,6 +59,43 @@ const CreateRoom = () => {
       data = { roomKey, nickname };
     }
     console.log(data);
+
+    const signInAnonymouslyFirebase = () => {
+      signInAnonymously(auth)
+        .then(() => {
+          console.log("Im Signed In");
+          // Signed in..
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // ...
+          console.log(errorCode, errorMessage);
+        });
+    };
+
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        // ...
+        writeUserData(uid, data.nickname, data.roomKey);
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+
+    const writeUserData = (userId, nickname, roomKey) => {
+      set(ref(db, "rooms/" + roomKey + "/players/" + userId), {
+        id: userId,
+        nickname,
+      });
+    };
+
+    signInAnonymouslyFirebase();
   };
 
   return (
