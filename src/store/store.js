a25fdf300bcefeee310af53d527d6ff89b8bf198
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-10-30 15:16:10 +0200

    added uiSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import databaseSlice from "./database-slice";
+import uiSlice from "./ui-slice";
 
 const store = configureStore({
-  reducer: { database: databaseSlice.reducer },
+  reducer: { database: databaseSlice.reducer, ui: uiSlice.reducer },
 });
 
 export default store;
