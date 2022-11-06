6a8a1b2036a9c7f2d259f58a916f07d94fab8dbb
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-11-06 11:08:13 +0200

    conditional rendering create and enter room

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,16 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  homepageShow: true,
+  welcomeDialogShow: true,
+  createRoomDialogShow: false,
+  enterRoomDialogShow: false,
+  mainShow: false,
+};
 
-  };
+const uiSlice = createSlice({
+  name: "ui",
+  initialState,
+  reducers: {
+    createRoom(state) {
+      state.createRoomDialogShow = true;
+      state.enterRoomDialogShow = false;
+    },
+    enterRoom(state) {
+      state.createRoomDialogShow = false;
+      state.enterRoomDialogShow = true;
+    },
+    startQuiz(state) {
+      state.createRoomDialogShow = false;
+      state.enterRoomDialogShow = false;
+      state.homepageShow = false;
+      state.mainShow = true;
+    },
+  },
+});
 
-  const uiSlice = createSlice({
-    name: "ui",
-    initialState,
-    reducers: {
-
-    }
-  });
-
-  export const uiActions = uiSlice.actions;
-  export default uiSlice;
+export const uiActions = uiSlice.actions;
+export default uiSlice;
