9f619f003cee9d074be163d62058f3ed89045ccd
Tomer Bourstein <88827223+tomerbourstein@users.noreply.github.com>
2022-11-30 16:47:44 +0200

    welcome, enter room and create room styling

--- src/components/Welcome/CreateRoom.js
+++ src/components/Welcome/CreateRoom.js
@@ -7,11 +7,13 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
-
+import Divider from '@mui/material/Divider';
+import InputAdornment from "@mui/material/InputAdornment";
 import { createRoomAndPlayers } from "../../util/Firebase";
 import { randomNumber } from "../../util/index";
 import Superheroes from "superheroes";
-// import classes from "./Homepage.module.css";
+import Hero from "../../images/hero-icon-50px.png";
+import classes from "./Homepage.module.css";
 
 const CreateRoom = () => {
   const [roomKey, setRoomKey] = useState("");
@@ -40,32 +42,55 @@ const CreateRoom = () => {
 
   const copyToClipboardHandler = (roomKey) => {
     navigator.clipboard.writeText(roomKey);
-  }
+  };
 
   return (
-    <Fragment>
-      <CardContent> 
-        <Typography color="text.secondary">
-          Share Room Key With Your Friends!
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <TextField
-          disabled
-          id="outlined-name"
-          label="Room Key"
-          value={roomKey}
-        />
-        <Button onClick={copyToClipboardHandler}> Copy </Button>
-      </CardActions>
-      <CardActions>
-        <TextField
-          id="outlined-name"
-          label="Choose Nickname"
-          value={!nickname && nickname !== "" ? generatedNickname : nickname}
-          onChange={nicknameChangeHandler}
-        />
-        <Button onClick={createRoomSubmitHandler}> Enter Room </Button>
+    <Fragment className={classes.createRoomButtons}>
+      {/* <CardContent className={classes.nonTextField}>
+        <Typography>Share With Friends!</Typography>
+      </CardContent> */}
+      <Divider variant="middle" />
+      <div className={classes.actionButtons}>
+        <CardActions>
+          <TextField
+            fullWidth
+            className={classes.TextField}
+            disabled
+            id="outlined-name"
+            label="Room Key"
+            value={roomKey}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end" className={classes.adornment}>
+                  <Button onClick={copyToClipboardHandler}> Copy </Button>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </CardActions>
+        <CardActions className={classes.nonTextField}>
+          {" "}
+          Share With Friends!
+        </CardActions>
+        <CardActions>
+          <TextField
+            fullWidth
+            id="outlined-name"
+            label="Choose Nickname"
+            value={!nickname && nickname !== "" ? generatedNickname : nickname}
+            onChange={nicknameChangeHandler}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end" className={classes.adornment}>
+                  <img src={Hero} className={classes.heroAvatar}/>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </CardActions>
+      </div>
+      <CardActions className={classes.enterRoom}>
+        <Button onClick={createRoomSubmitHandler}> Create Room </Button>
       </CardActions>
     </Fragment>
   );
